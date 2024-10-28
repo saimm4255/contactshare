4b680517f916ea50feddf26a50500fc2bf1952f8
Muhammad Saem <saimm4255@gmail.com>
2024-10-28 23:28:42 +0500

    Custom authentication 1

--- main/permissions.py
+++ main/permissions.py
@@ -2,9 +2,7 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsOwnerOrReadOnly(BasePermission):
-    """
-    Custom permission to only allow owners of an object to edit it.
-    """
+
     def has_object_permission(self, request, view, obj):
         
         if request.method in SAFE_METHODS:
